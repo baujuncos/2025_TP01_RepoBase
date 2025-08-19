d51e396a2ce3f33322595be07fbd270401c4bebf
baujuncos <baujuncos@gmail.com>
2025-08-19 19:41:17 -0300

    Func2: se agrego funcion de resta también

--- src/app.js
+++ src/app.js
@@ -5,12 +5,17 @@ function saludar() {
 
 saludar();
 
-// Función simple: sumar dos números
+// Función simple: sumar y restar dos números
 
 function suma(a, b) {
   return a + b;
 }
 
-console.log("Resultado de 2 + 3 =", suma(2, 3));
+function resta(a, b) {
+  return a - b;
+}
 
-module.exports = { suma };
+console.log("Resultado de 2 + 3 =", suma(2, 3));
+console.log("Resultado de 5 - 2 =", resta(5, 2));
+
+module.exports = { suma, resta };
