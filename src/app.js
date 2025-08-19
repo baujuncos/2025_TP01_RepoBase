df24fb6aa7922bdf817b1a0f2bbb727e8d43cfc9
baujuncos <baujuncos@gmail.com>
2025-08-19 19:40:15 -0300

    Func1: se agrego funcion de suma

--- src/app.js
+++ src/app.js
@@ -4,3 +4,13 @@ function saludar() {
 }
 
 saludar();
+
+// Función simple: sumar dos números
+
+function suma(a, b) {
+  return a + b;
+}
+
+console.log("Resultado de 2 + 3 =", suma(2, 3));
+
+module.exports = { suma };
